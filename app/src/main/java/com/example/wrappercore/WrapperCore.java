6bdf7b28e2eadec40f502d3b8f88352d66cd49a2
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-02-25 18:39:37 +0200

    refactor: wrapperCore

--- app/src/main/java/com/example/wrappercore/WrapperCore.java
+++ app/src/main/java/com/example/wrappercore/WrapperCore.java
@@ -12,11 +12,14 @@ public class WrapperCore {
   private final ControlManager controlManager;
   private final ModelController modelController;
 
+  //FIXME: put the correct PROD url
+  private final String modelUrl = "https://learny-v1.onrender.com/api/v1/downloadModel";
+
   //FIXME: This constructor is not the production one it is for testing purposes only
   public WrapperCore() {
     this.controlManager = new ControlManager();
     this.mindWaveMobile2 = new MindWaveMobile2();
-    this.modelController = new ModelController("https://learny-v1.onrender.com/api/v1/downloadModel");
+    this.modelController = new ModelController(modelUrl);
     this.mindWaveMobile2.addEventListener(controlManager.getModeManager());
     this.mindWaveMobile2.addEventListener(this.modelController);
   }
@@ -25,11 +28,10 @@ public class WrapperCore {
   public WrapperCore(BluetoothManager bluetoothManager, String macAddress) {
     this.controlManager = new ControlManager();
     this.mindWaveMobile2 = new MindWaveMobile2(bluetoothManager, macAddress);
-    this.modelController = new ModelController("https://learny-v1.onrender.com/api/v1/downloadModel");
+    this.modelController = new ModelController(modelUrl);
     this.mindWaveMobile2.connect();
     this.mindWaveMobile2.addEventListener(controlManager.getModeManager());
     this.mindWaveMobile2.addEventListener(this.modelController);
-
   }
 
   public void addListener(EventListener listener) {
@@ -49,7 +51,7 @@ public class WrapperCore {
   public ControlManager getControlManager() {
     return controlManager;
   }
-  
+
   //FIXME: This method is for testing purposes only
   public ModelController getModelController() {
     return modelController;
