4e388d020ad8dfaaa48ab3a1d658ba45af36ff8e
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-07-22 22:54:13 +0300

    chore!: prepare main for pub

--- app/src/main/java/com/example/wrappercore/WrapperCore.java
+++ app/src/main/java/com/example/wrappercore/WrapperCore.java
@@ -18,9 +18,24 @@ public class WrapperCore {
   private final ControlManager controlManager;
   private final ModelController modelController;
   private final WheelchairController wheelchairController;
+  //NOTE: if you want to use your own ai-model
+  //NOTE: change this to your own ai-model link in .tflite formate 
+  //NOTE: and change the io vectors in ai component correspondingly.
   private final String modelUrl = "https://learny-v1.onrender.com/api/v1/downloadModel";
 
 
+  public WrapperCore(BluetoothManager bluetoothManager, String macAddress)
+      throws IOException {
+    this.controlManager = new ControlManager();
+    this.modelController = new ModelController(this.modelUrl);
+    this.modelController.addListener(this.controlManager.getActionManager());
+    this.headsetController = new HeadsetController(bluetoothManager, macAddress);
+    this.headsetController.connect();
+    this.headsetController.addEventListener(this.controlManager.getBlinkManager());
+    this.headsetController.addEventListener(this.modelController);
+  }
+
+  //NOTE: this constructor is meant for the users who have the hardware (wheelchair) serial connection
   public WrapperCore(BluetoothManager bluetoothManager, String macAddress, UsbManager usbManager)
       throws IOException {
     this.controlManager = new ControlManager();
@@ -49,19 +64,20 @@ public class WrapperCore {
     }
   }
 
-  public void makeWheelchairGoForward() {
-    wheelchairController.forward();
-  }
+  //NOTE: enable if you have hardware serial connection
+  // public void makeWheelchairGoForward() {
+  //   assert wheelchairController.forward():"Connection is null";
+  // }
 
-  public void makeWheelchairGoLeft() {
-    wheelchairController.left();
-  }
+  // public void makeWheelchairGoLeft() {
+  //   assert wheelchairController.left():"Connection is null";
+  // }
 
-  public void makeWheelchairGoRight() {
-    wheelchairController.right();
-  }
+  // public void makeWheelchairGoRight() {
+  //   assert wheelchairController.right():"Connection is null";
+  // }
 
-  public void makeWheelchairStop() {
-    wheelchairController.stop();
-  }
+  // public void makeWheelchairStop() {
+  //   assert wheelchairController.stop():"Connection is null";
+  // }
 }
